ec2b495048bf07edd528a9e43ffa4e8cca7c0841
OBPSG <vbeasupergamer@gmail.com>
2022-06-14 14:09:34 -0500

    Got the first generate_points test passing

--- fetch_rewards_mle_take_home_image_coordinates/calculator.py
+++ fetch_rewards_mle_take_home_image_coordinates/calculator.py
@@ -16,4 +16,17 @@ class Point_Set_Simensions(BaseModel):
     dimensions: Dimensions
 
 def generate_points(min_x: float, max_x: float, min_y: float, max_y: float, num_cols: int, num_rows: int):
-    return None
+    point_set = []
+    x_step = (max_x - min_x)/(num_cols - 1)
+    y_step = (max_y - min_y)/(num_rows - 1) 
+    y_pos = min_y
+    for i in range(num_rows):
+        point_set.append([])
+        x_pos = min_x
+        for j in range(num_cols):
+            point_tuple = (x_pos, y_pos)
+            point_set[i].append(point_tuple)
+            x_pos += x_step
+        y_pos += y_step
+
+    return point_set
--- fetch_rewards_mle_take_home_image_coordinates/calculator_test.py
+++ fetch_rewards_mle_take_home_image_coordinates/calculator_test.py
@@ -3,9 +3,9 @@ import calculator
 
 class Test_calculator_test(unittest.TestCase):
     def test_1(self):
-        result = [[1.0, 1.0], [2.0, 1.0], [3.0, 1.0],
-                  [1.0, 2.0], [2.0, 2.0], [3.0, 2.0],
-                  [1.0, 3.0], [2.0, 3.0], [3.0, 3.0]]
+        result = [[(1.0, 1.0), (2.0, 1.0), (3.0, 1.0)],
+                  [(1.0, 2.0), (2.0, 2.0), (3.0, 2.0)],
+                  [(1.0, 3.0), (2.0, 3.0), (3.0, 3.0)]]
 
         self.assertListEqual(result, calculator.generate_points(1.0, 3.0, 1.0, 3.0, 3, 3))
 

3343af0d4fab0ed71c7b121af2db6c17c86c8a6f
OBPSG <vbeasupergamer@gmail.com>
2022-06-16 11:37:58 -0500

    Made additions to the calculator endpoint

--- fetch_rewards_mle_take_home_image_coordinates/calculator.py
+++ fetch_rewards_mle_take_home_image_coordinates/calculator.py
@@ -8,32 +8,40 @@ class Dimensions(BaseModel):
     height: int
     width: int
 
-class Point_Set_Simensions(BaseModel):
+class Point_Set_Dimensions(BaseModel):
     p1: Point
     p2: Point
     p3: Point
     p4: Point
     dimensions: Dimensions
 
-"""
-This function takes in a list of four point tuples that represent the corner points of an image area rectangle
-ex: [(0, 0), (3, 0), (0, 3), (3, 3)]
-and returns a tuple containing the minimum and maximum x and y coordinates
-"""
+class Bounds_Set(BaseModel):
+    minX: float
+    maxX: float
+    minY: float
+    maxY: float
+
+
 def min_max_xy(points):
+    """
+    This function takes in a list of four point tuples that represent the corner points of an image area rectangle
+    ex: [(0, 0), (3, 0), (0, 3), (3, 3)]
+    and returns a tuple containing the minimum and maximum x and y coordinates
+    """
     return (min(points[0][0], points[1][0], points[2][0], points[3][0]),
             max(points[0][0], points[1][0], points[2][0], points[3][0]),
             min(points[0][1], points[1][1], points[2][1], points[3][1]),
             max(points[0][1], points[1][1], points[2][1], points[3][1])
     )
 
-"""
-Function generate_points is called after min_max_xy
-Takes in a set of four floats representing the minimum and maximum x and y coordinates,
-plus a pair of ints for the number of rows and columns in the pixel grid
-and returns a two-dimensional list of point tuples for the final pixel coordinates
-"""
+
 def generate_points(min_x: float, max_x: float, min_y: float, max_y: float, num_cols: int, num_rows: int):
+    """
+    Function generate_points is called after min_max_xy
+    Takes in a set of four floats representing the minimum and maximum x and y coordinates,
+    plus a pair of ints for the number of rows and columns in the pixel grid
+    and returns a two-dimensional list of point tuples for the final pixel coordinates
+    """
     point_set = []
     x_step = (max_x - min_x)/(num_cols - 1)
     y_step = (max_y - min_y)/(num_rows - 1) 
--- fetch_rewards_mle_take_home_image_coordinates/main.py
+++ fetch_rewards_mle_take_home_image_coordinates/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
+from pydantic.types import Json
+import calculator
+import json
 
 app = FastAPI()
 
@@ -12,3 +16,39 @@ async def root():
 async def intro():
     return {"message": "Use Postman, a similar web API driver, or navigate to the /docs# endpoint to make a POST request"}
 
+"""
+The expected format for the JSON object of the request body is as follows
+{
+    //corner points
+    "p1": {
+        "x": integer for the x coodinate
+        "y": integer for the y coordinate
+    },
+    "p2" : {
+        "x": integer for the x coodinate
+        "y": integer for the y coordinate
+    },
+    "p3": {
+        "x": integer for the x coodinate
+        "y": integer for the y coordinate
+    },
+    "p4": {
+        "x": integer for the x coodinate
+        "y": integer for the y coordinate
+    }
+
+    dimensions; {
+        "height": integer representing the number of pixel rows in the image grid
+        "width": integer representing the number of pixel columns in the image grid
+    }
+}
+"""
+@app.post("/image_coordinates")
+async def calculate(input: calculator.Point_Set_Dimensions):
+    bounds_tuple = calculator.min_max_xy([
+        (input.p1.x, input.p1.y),
+        (input.p2.x, input.p2.y),
+        (input.p3.x, input.p3.y),
+        (input.p4.x, input.p4.y)])
+
+    return {"message": str(bounds_tuple)}
